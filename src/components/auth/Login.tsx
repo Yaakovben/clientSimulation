0a5fc1e08d074de3d058e3b18225dacfb9ec0499
Yaakovben <yaakovbenyizchak1@gmail.com>
2024-11-14 19:44:01 +0200

    Getting started with Navigate for routing

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../../redux/store'
+import { fetchLogin } from '../../redux/slices/userSlice'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function Login() {
+    const dispatch = useAppDispatch()
+    const [username,setUserName] = useState("")
+    const [password,setPassword] = useState("")
+    const {user} = useAppSelector(state=>state.user)
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+      if(!user?._id ) return
+      navigate('/atteck')
+
+    },[user])
+
+    useEffect(()=>{
+      if(user?._id){
+        navigate('/atteck')
+      }
+    },[])
+    
   return (
-    <div>Login</div>
+
+    <div className='login'>
+      <div className='login-form'>
+        <h3>Login</h3>
+        <input type='text' 
+        placeholder='User Name' 
+        value={username} 
+        onChange={(e)=>setUserName(e.target.value)}/>
+        <input type='password' 
+        placeholder='Password' 
+        value={password} 
+        onChange={(e)=>setPassword(e.target.value)}/>
+        <button onClick={()=>{  dispatch(fetchLogin({username,password})), console.log("llll");
+        }}>Login</button>
+        </div>
+    </div>
   )
 }
