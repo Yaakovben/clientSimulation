29658d776e0de650f177c77d41812cb68d96353c
Yaakovben <yaakovbenyizchak1@gmail.com>
2024-11-17 16:42:39 +0200

    Protection display component handling

--- src/components/pages/defense/DefensiveDisplay.tsx
+++ src/components/pages/defense/DefensiveDisplay.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { socket } from '../../../main';
 import { IMissileList } from '../../../types/user';
+import { useAppSelector } from '../../../redux/store';
 
 export default function DefensiveDisplay() {
+  const user = useAppSelector(state=>state.user.user)
   const [missileList, setMissileList] = useState<IMissileList[]>([]); 
-
   useEffect(() => {
+    socket.emit("start")
     socket.on("listMissiles", (missilies:IMissileList[]) => {
       setMissileList(missilies);
     });
-  }, []);
+  }, [missileList]);
+  const HandelIntercepted = (missileLocation:string,missileName:string,userName:any)=>{
+    socket.emit("defensiveFire",missileLocation,missileName,userName)
+  }
   return (
     <div>
       <table className='table'>
@@ -27,6 +32,7 @@ export default function DefensiveDisplay() {
                 <td>{missile.name}</td>
                 <td>{missile.time}</td> 
                 <td>{missile.status}</td>
+                <td><button onClick={()=>HandelIntercepted(missile.location,missile.name,user?.username)!} >🚀</button></td>
               </tr>
             ))
           ) : <p>Not have your missiles 🧐</p>}
