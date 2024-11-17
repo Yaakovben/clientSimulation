bc60d798c4e593fd322035e83dd7fff51330a490
Yaakovben <yaakovbenyizchak1@gmail.com>
2024-11-17 15:29:29 +0200

    Creating an initial table for attack display

--- src/components/pages/attack/AttackDisplay.tsx
+++ src/components/pages/attack/AttackDisplay.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { socket } from '../../../main';
+import { IMissileList } from '../../../types/user'
 
 export default function AttackDisplay() {
+  
+  const [missileList, setMissileList] = useState<IMissileList[]>([]); 
+
+  useEffect(() => {
+    socket.on("listMissiles", (MissileList) => {
+      setMissileList(MissileList);
+    });
+  }, [missileList]);
+
   return (
-    <div>AttackDisplay</div>
-  )
+    <div>
+      <table className='table'>
+        <thead className='theadTable'>
+          <tr>
+            <th>Name</th>
+            <th>time</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missileList.length > 0 ? (
+            missileList.map((missile, index) => (
+              <tr key={index}>
+                <td>{missile.name}</td>
+                <td>{missile.time}</td> 
+                <td>{missile.status}</td>
+              </tr>
+            ))
+          ) : <p>Not have your missiles 🧐</p>}
+        </tbody>
+      </table>
+    </div>
+  );
 }
